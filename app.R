8a2659d10f08aa2dff6081cb2364afa66445c206
Edward Jin <68412081+EdwardJ1n@users.noreply.github.com>
2021-12-27 14:01:18 +0800

    Copy the example code

--- app.R
+++ app.R
@@ -6,55 +6,43 @@
 #
 #    http://shiny.rstudio.com/
 #
-
-# Create the manifest
-rsconnect::writeManifest()
-
 library(shiny)
-library(magrittr)
-
-# Application title
-title  <- "Old Faithful Geyser Data" %>% 
-            titlePanel()
-
-# Sidebar with a slider input for number of bins 
-side   <- sliderInput("bins",
-                      "Number of bins:",
-                      min = 1,
-                      max = 50,
-                      value = 30) %>%   
-            sidebarPanel()
-
-# Show a plot of the generated distribution
-main   <- plotOutput("distPlot") %>% 
-            mainPanel()
-
-# Combine sidebar and main panels into layout
-layout <- sidebarLayout(side, 
-                        main)
 
 # Define UI for application that draws a histogram
-ui     <- fluidPage(title, 
-                    layout)
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+
     output$distPlot <- renderPlot({
-       # Generate bins based on input$bins from ui.R
-       bins <- input$bins + 1
-        
-       # Draw the histogram with the specified number of bins
-       faithful[, 2] %>% 
-         hist(breaks = seq(min(.), 
-                           max(.), 
-                           length.out = bins), 
-              col = 'darkgray', 
-              border = 'white')
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
 }
 
 # Run the application 
-shinyApp(ui = ui, 
-         server = server)
-
+shinyApp(ui = ui, server = server)
